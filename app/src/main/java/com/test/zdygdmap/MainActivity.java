783fa4bc26fdfc8f3bc67bbf6d73d3895e689eb8
DavyJohn <1139099003@qq.com>
2017-11-07 17:47:24 +0800

    Initial commit

---
+++ app/src/main/java/com/test/zdygdmap/MainActivity.java
@@ -0,0 +1,237 @@
+package com.test.zdygdmap;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.LocationSource;
+import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.UiSettings;
+import com.amap.api.maps2d.model.BitmapDescriptorFactory;
+import com.amap.api.maps2d.model.LatLng;
+import com.amap.api.maps2d.model.Marker;
+import com.amap.api.maps2d.model.MarkerOptions;
+import com.amap.api.maps2d.model.MyLocationStyle;
+import com.test.zdygdmap.adapter.InfoWinAdapter;
+import com.test.zdygdmap.entity.Constant;
+import com.test.zdygdmap.utils.CheckPermissionsActivity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
+
+
+public class MainActivity extends CheckPermissionsActivity implements AMap.OnMapClickListener, AMap.OnMarkerClickListener, AMapLocationListener, LocationSource {
+
+    private MapView mapView;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient;
+    //声明AMapLocationClientOption对象
+    private AMapLocationClientOption mLocationOption;
+    private AMap aMap;
+    private UiSettings uiSettings;
+    private InfoWinAdapter adapter;
+    private Marker oldMarker;
+    private OnLocationChangedListener mListener;
+    private LatLng myLatLng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        //在执行onCreateView时执行mMapView.onCreate(savedInstanceState)，实现地图生命周期管理
+        mapView.onCreate(savedInstanceState);
+        initOperation();
+        String sha1 = sHA1(this);
+        System.out.print(sha1);
+    }
+
+    private void initView() {
+        mapView = (MapView) initV(R.id.mapView);
+    }
+
+    private void initOperation() {
+        initMap();
+        initLocation();
+    }
+
+    /**
+     * 初始化地图
+     */
+    private void initMap() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+            uiSettings = aMap.getUiSettings();
+            aMap.setOnMapClickListener(this);
+        }
+        uiSettings.setZoomControlsEnabled(false); //隐藏缩放控件
+        //自定义InfoWindow
+        aMap.setOnMarkerClickListener(this);
+        adapter = new InfoWinAdapter();
+        aMap.setInfoWindowAdapter(adapter);
+        addMarkerToMap(Constant.CHENGDU,"成都","中国四川省成都市");
+    }
+
+
+    /**
+     * 初始化定位
+     */
+    private void initLocation(){
+        // 自定义系统定位小蓝点--我的位置
+        MyLocationStyle myLocationStyle = new MyLocationStyle();
+        myLocationStyle.myLocationIcon(BitmapDescriptorFactory
+                .fromResource(R.drawable.mylocation));// 设置小蓝点的图标
+        myLocationStyle.strokeColor(getResources().getColor(R.color.blue));// 设置圆形的边框颜色
+        myLocationStyle.radiusFillColor(Color.argb(100, 29, 161, 242));// 设置圆形的填充颜色
+        myLocationStyle.strokeWidth(1.0f);// 设置圆形的边框粗细
+        aMap.setMyLocationStyle(myLocationStyle);
+        aMap.setLocationSource(this);// 设置定位资源。如果不设置此定位资源则定位按钮不可点击。并且实现activate激活定位,停止定位的回调方法
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+        aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+    }
+    //激活定位
+    //记得注册定位
+    //<service android:name="com.amap.api.location.APSService"/>
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        mListener = onLocationChangedListener;
+        if (mLocationClient == null) {
+            mLocationClient = new AMapLocationClient(this);
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mLocationClient.setLocationListener(this);
+            //设置为高精度定位模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            mLocationOption.setOnceLocation(true);
+            //设置是否返回地址信息（默认返回地址信息）
+            mLocationOption.setNeedAddress(true);
+            //设置定位参数
+            mLocationClient.setLocationOption(mLocationOption);
+            mLocationClient.startLocation();
+        }
+    }
+
+    //停止定位
+    @Override
+    public void deactivate() {
+        mListener = null;
+        if (mLocationClient != null) {
+            mLocationClient.stopLocation();
+            mLocationClient.onDestroy();
+        }
+        mLocationClient = null;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume(); //管理地图的生命周期
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause(); //管理地图的生命周期
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy(); //管理地图的生命周期
+    }
+
+    //地图的点击事件
+    @Override
+    public void onMapClick(LatLng latLng) {
+        //点击地图上没marker 的地方，隐藏inforwindow
+        if (oldMarker != null) {
+            oldMarker.hideInfoWindow();
+            oldMarker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker_normal));
+        }
+    }
+
+    //maker的点击事件
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        marker.showInfoWindow();
+        if (!marker.getPosition().equals(myLatLng)){ //点击的marker不是自己位置的那个marker
+            if (oldMarker != null) {
+                oldMarker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker_normal));
+            }
+            oldMarker = marker;
+            marker.setIcon(BitmapDescriptorFactory.fromResource(R.drawable.marker_selected));
+        }
+
+        return false; //返回 “false”，除定义的操作之外，默认操作也将会被执行
+    }
+
+    //定位成功的监听
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+
+        if (aMapLocation != null) {
+
+            if(mListener != null){
+//                aMap.clear();  清除之前的marker
+                mListener.onLocationChanged(aMapLocation);// 显示系统小蓝点-我的位置
+            }
+
+            if (aMapLocation.getErrorCode() == 0) {
+                myLatLng = new LatLng(aMapLocation.getLatitude(), aMapLocation.getLongitude());
+                aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatLng, 14));
+                String city = aMapLocation.getCity();
+                String address = aMapLocation.getAddress();
+//                addMarkerToMap(myLatLng,city,address);
+            } else {
+                //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError", "location Error, ErrCode:" + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }
+    }
+
+    private void addMarkerToMap(LatLng latLng, String title, String snippet) {
+        aMap.addMarker(new MarkerOptions().anchor(0.5f, 0.5f)
+                .position(latLng)
+                .title(title)
+                .snippet(snippet)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker_normal))
+        );
+    }
+    public static String sHA1(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), PackageManager.GET_SIGNATURES);
+            byte[] cert = info.signatures[0].toByteArray();
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(cert);
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < publicKey.length; i++) {
+                String appendString = Integer.toHexString(0xFF & publicKey[i])
+                        .toUpperCase(Locale.US);
+                if (appendString.length() == 1)
+                    hexString.append("0");
+                hexString.append(appendString);
+                hexString.append(":");
+            }
+            String result =  hexString.toString();
+            return result.substring(0, result.length()-1);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
